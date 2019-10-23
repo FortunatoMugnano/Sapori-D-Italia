d31cc75e9b6c781144d0a994142870019c76748b
FortunatoMugnano <fortunato.mugnano88@gmail.com>
2019-10-23 15:24:14 -0500

    added rating

--- sapori-ditalia/src/components/recipe/RecipeCard.js
+++ sapori-ditalia/src/components/recipe/RecipeCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Rating } from 'semantic-ui-react'
 
 
 class RecipeCard extends Component {
@@ -13,7 +14,40 @@ class RecipeCard extends Component {
                     <h3>Ingredients:</h3><p> {this.props.recipe.ingredients}</p>
                     <h3>Directions:</h3><p> {this.props.recipe.direction}</p>
                     <h3>Difficulty:</h3><p>{this.props.recipe.difficulty}</p>
-                    <h3>Rate:</h3><p> {this.props.recipe.rate}</p>
+                    <h3>Rate:</h3><div className="ui star rating" role="radiogroup" tabIndex="-1">
+                        <i
+                            tabIndex="0"
+                            aria-checked="false"
+                            aria-posinset="1"
+                            aria-setsize="4"
+                            className="active icon"
+                            role="radio"
+                        ></i>
+                        <i
+                            tabIndex="0"
+                            aria-checked="false"
+                            aria-posinset="2"
+                            aria-setsize="4"
+                            className="active icon"
+                            role="radio"
+                        ></i>
+                        <i
+                            tabIndex="0"
+                            aria-checked="false"
+                            aria-posinset="3"
+                            aria-setsize="4"
+                            className="active icon"
+                            role="radio"
+                        ></i>
+                        <i
+                            tabIndex="0"
+                            aria-checked="true"
+                            aria-posinset="4"
+                            aria-setsize="4"
+                            className="icon"
+                            role="radio"
+                        ></i>
+                    </div><div>{this.props.recipe.rate}</div>
                     <button
                         className="ui icon button"
                     ><i aria-hidden="true" className="add icon"></i>
